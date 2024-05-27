3c5853dac04ef7ef1e61f5b64db439636f67429d
Alen Lobeiras <alenlobeiras@Alens-MacBook-Pro.local>
2024-05-27 20:07:39 +0200

    movidas

--- firebase-config.js
+++ firebase-config.js
@@ -9,7 +9,8 @@ const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID // Incluye esto si usas measurementId
 };
 
 firebase.initializeApp(firebaseConfig);
